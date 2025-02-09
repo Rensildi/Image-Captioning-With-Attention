6b08685e52f6999b7be2b7169c7c951cc04b5e30
Rensildi <rensildi.kalanxhi1@gmail.com>
2025-02-09 17:50:30 -0500

    1. training.py - included the FlickrDataset.

--- training.py
+++ training.py
@@ -1,36 +1,125 @@
+import os
 import torch
+import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import torchvision.transforms as T
+from torch.utils.data import Dataset, DataLoader
+from collections import Counter
+import nltk
+from nltk.tokenize import word_tokenize
+from PIL import Image
 import matplotlib.pyplot as plt
-import numpy as np
 
+# Initialize NLTK
+nltk.download('punkt')
 
-# Ensure `vocab` is defined
-dataset = FlickrDataset(
-    root_dir = data_location + "/Images",
-    captions_file = data_location + "/captions.txt",
-    transform = transforms
-)
+# Vocabulary Class
+class Vocabulary:
+    def __init__(self, freq_threshold):
+        self.itos = {0: "<PAD>", 1: "<SOS>", 2: "<EOS>", 3: "<UNK>"}
+        self.stoi = {v: k for k, v in self.itos.items()}
+        self.freq_threshold = freq_threshold
 
+    def __len__(self):
+        return len(self.itos)
+
+    @staticmethod
+    def tokenize(text):
+        return word_tokenize(text.lower())
+
+    def build_vocab(self, sentence_list):
+        frequencies = Counter()
+        idx = 4  # Start indexing from 4 after reserved tokens
+        for sentence in sentence_list:
+            for word in self.tokenize(sentence):
+                frequencies[word] += 1
+                if frequencies[word] == self.freq_threshold:
+                    self.stoi[word] = idx
+                    self.itos[idx] = word
+                    idx += 1
+
+    def numericalize(self, text):
+        tokenized_text = self.tokenize(text)
+        return [self.stoi.get(token, self.stoi["<UNK>"]) for token in tokenized_text]
+
+# Flickr Dataset Class
+class FlickrDataset(Dataset):
+    def __init__(self, root_dir, captions_file, transform=None, freq_threshold=5):
+        self.root_dir = root_dir
+        self.df = pd.read_csv(captions_file)
+        self.transform = transform
+        self.imgs = self.df["image"]
+        self.captions = self.df["caption"]
+
+        self.vocab = Vocabulary(freq_threshold)
+        self.vocab.build_vocab(self.captions.tolist())
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        caption = self.captions[idx]
+        img_name = self.imgs[idx]
+        img_location = os.path.join(self.root_dir, img_name)
+        img = Image.open(img_location).convert("RGB")
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        caption_vec = [self.vocab.stoi["<SOS>"]]
+        caption_vec += self.vocab.numericalize(caption)
+        caption_vec += [self.vocab.stoi["<EOS>"]]
+
+        return img, torch.tensor(caption_vec)
+
+# Define transformations
+transforms = T.Compose([T.Resize((224, 224)), T.ToTensor()])
+
+# Initialize dataset and vocab
+dataset = FlickrDataset(root_dir="./Images", captions_file="./captions.txt", transform=transforms)
+
+# Now you can access dataset.vocab to get vocab for the model
 vocab = dataset.vocab
+vocab_size = len(vocab)
 
+# Model class
+class ImageCaptioningModel(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size):
+        super(ImageCaptioningModel, self).__init__()
+        self.embed_size = embed_size
+        self.hidden_size = hidden_size
 
-vocab_size = len(vocab)  # This should be your vocab size, e.g., len(dataset.vocab)
+        # Image feature extractor (e.g., ResNet-152 or your own CNN)
+        self.encoder = nn.Conv2d(3, self.embed_size, kernel_size=3, stride=2, padding=1)
 
-# Initialize the model
+        # Decoder (LSTM)
+        self.decoder = nn.LSTM(self.embed_size, self.hidden_size, batch_first=True)
+
+        # Linear layer to predict words
+        self.fc = nn.Linear(self.hidden_size, vocab_size)
+
+    def forward(self, images, captions):
+        features = self.encoder(images)
+        features = features.view(features.size(0), -1)  # Flatten
+
+        hiddens, _ = self.decoder(captions)
+        output = self.fc(hiddens)
+
+        return output
+
+# Initialize model
 model = ImageCaptioningModel(embed_size=256, hidden_size=512, vocab_size=vocab_size)
 
-# Choose the device (GPU or CPU)
+# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
-# Loss Function (Cross-Entropy Loss)
-criterion = torch.nn.CrossEntropyLoss()
-
-# Optimizer (Adam)
+# Optimizer and loss function
+criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-# Training Procedure
+# Train function
 def train_model(data_loader, num_epochs=10):
     model.train()
     train_losses = []
@@ -61,7 +150,6 @@ def train_model(data_loader, num_epochs=10):
 
     return train_losses
 
-
 # Plot training loss
 def plot_loss(train_losses):
     plt.plot(range(1, len(train_losses) + 1), train_losses)
@@ -70,7 +158,9 @@ def plot_loss(train_losses):
     plt.title('Training Loss Over Epochs')
     plt.show()
 
-# Assuming you have a DataLoader `data_loader`
-# Train the model for 10 epochs and plot the training loss
+# Create data loader
+data_loader = DataLoader(dataset=dataset, batch_size=64, shuffle=True)
+
+# Train model for 10 epochs
 train_losses = train_model(data_loader, num_epochs=10)
 plot_loss(train_losses)
